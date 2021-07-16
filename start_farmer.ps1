f8e6497ae8923632c0d2cda7c1fd388e1a3a12fd
nahvan <82133693+nahvan@users.noreply.github.com>
2021-07-15 23:20:47 -0400

    Updated comments and interface output

--- start_farmer.ps1
+++ start_farmer.ps1
@@ -10,20 +10,24 @@ $blockchain_list =
  "C:\Users\Nathan3\AppData\Local\hddcoin-blockchain\app-1.2.115\resources\app.asar.unpacked\daemon\hddcoin.exe",
  "C:\Users\Nathan3\AppData\Local\greendoge-blockchain\app-1.0.2104\resources\app.asar.unpacked\daemon\greendoge.exe",
  "C:\Users\Nathan3\AppData\Local\flora-blockchain\app-0.2.3\resources\app.asar.unpacked\daemon\flora.exe",
- "C:\Users\Nathan3\AppData\Local\chiadoge\app-1.2.20\resources\app.asar.unpacked\daemon\chiadoge.exe",
+ "C:\Users\Nathan3\AppData\Local\chiadoge\app-1.2.30\resources\app.asar.unpacked\daemon\chiadoge.exe",
  "C:\Users\Nathan3\AppData\Local\dogechia-blockchain\app-0.1.4\resources\app.asar.unpacked\daemon\dogechia.exe",
  "C:\Users\Nathan3\AppData\Local\kale-blockchain\app-0.1.180\resources\app.asar.unpacked\daemon\kale.exe",
  "C:\Users\Nathan3\AppData\Local\avocado-blockchain\app-1.1.7120\resources\app.asar.unpacked\daemon\avocado.exe"
  )
 
+##list of fork names (in the same order as blockchain list)
 $fork_names = ("Chia", "Flax", "Goji", "Chaingreen", "Spare", "Seno", "Equality", "HDDCoin", "GreenDoge", "Flora", "ChiaDoge", "DogeChia", "Kale", "Avocado")
+
+$check_interval = 900 #farm summary interval - put in config later
+
+####-----------------------------------Change the above to your config/preferences-----------------------------------####
+
 
 ##get user imputs (put this inside a while loop later)
 write "functions: 'start farmer', 'start harvester', 'farm summary full', 'farm summary short'"
 
 $function_run = read-host "What would you like to run?"
-
-$check_interval = 900 #farm summary interval - put in config later
 
 #starts farmer for each blockchain
 function start_farmer{
@@ -34,7 +38,7 @@ function start_farmer{
         start-sleep 20 #wait 20 seconds between instances
     }
 
-    #$function_run = read-host "What would you like to run?"
+    write "All fork farmers launched!"
 }
 
 #starts harvester for each blockchain
@@ -45,7 +49,7 @@ function start_harvester{
         start-sleep 5 #wait 5 seconds between instances
     }
     
-    #$function_run = read-host "What would you like to run?"
+    write "All fork harvesters launched!"
 }
 
 #gets farm summary of each blockchain
@@ -70,7 +74,9 @@ function farm_summary_full{
         $current_time = (Get-Date -f HH:mm)
         $current_date = (Get-Date -f MM"/"dd)
         write-host "Last updated:" $current_date", "$current_time
-        #$function_run = read-host "What would you like to run?"
+        $wait_time = $check_interval/60
+
+        write-host "Next check in"$wait_time "minutes..."
         start-sleep $check_interval
     }
 }
@@ -91,11 +97,10 @@ function farm_summary_condensed{
             $status = $farmed_fork[0].Substring(16)
             $farm_status = $fork_names[$i] + " Status: " + $status
             $summary += $farm_status
-            $summary += $farmed_fork[1].Substring(6)
+            $summary += $farmed_fork[1].Substring(6) 
             $summary += $wallet_fork[4].Substring(10)
             $summary += ""
         
-
         }  
     
     write $summary
